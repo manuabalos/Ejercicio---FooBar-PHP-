394ed5e22b1521c6ed02de793216fb8f3e2eb577
Manuel Ábalos Serrano <manu.abalos@gmail.com>
2015-09-25 17:11:44 +0200

    Creacion de la clase JUEGO y su metodo Comenzar

--- ejercicio.php
+++ ejercicio.php
@@ -1,7 +1,16 @@
 <?php 
 
-	for($i=1;$i<=100;$i++){
-		echo $i."<br>";
-	}
+	class Juego{
+
+ 		function comenzar(){
+ 			for($i=1;$i<=100;$i++){
+ 				echo $i."<br>";
+ 			}
+ 		}
+ 		
+ 	}
+
+ 	$mi_juego = new Juego();
+ 	$mi_juego->comenzar();
 
 ?>
